230b67ff662f7216976eab92421b946122d5282e
Raine Taylor <trainewillow@gmail.com>
2020-07-21 02:23:24 -0500

    added primatives for band and rect

--- src/2d_primitives.glsl
+++ src/2d_primitives.glsl
@@ -12,6 +12,19 @@ float draw_circle(vec2 curr_pixel, vec2 c_coords, float radius, float blur, floa
 	return calcCircle * alpha;
 }
 
+float draw_band(float axis, float start, float end, float blur, float alpha) {
+	float step1 = smoothstep(start-(0.0004+blur), start+(0.0004+blur), axis);
+	float step2 = smoothstep(end+(0.0004+blur), end-(0.0004+blur), axis);
+
+	return step1 * step2 * alpha;
+}
+
+float draw_rect(vec2 uv, vec2 c_coords, float width, float height, float blur, float alpha) {
+	float band1 = draw_band(uv.x, c_coords.x, c_coords.x + width, blur, alpha);
+	float band2 = draw_band(uv.y, c_coords.y, c_coords.y + height, blur, alpha);
+	return band1 * band2;
+}
+
 vec3 set_color(float shape, vec3 colors) {
 	vec3 ret_color = vec3(0.0);
 
@@ -30,14 +43,17 @@ void main() {
 	uv.x *= u_resolution.x/u_resolution.y;
 	uv.y *= u_resolution.y/u_resolution.x;
 
-	for (float i = 0.0; i < 6.0; i += 0.2) {
-		float circ1 = draw_circle(uv, vec2(abs(sin(u_time*0.7)), i), 0.01*sin(u_time)+0.1, uv.x*0.04, 1.0);
+	float circle = draw_circle(uv, vec2(0.6, 0.6), 0.2, 0.0, 1.0);
+	color -= circle*color;
+	color += set_color(circle, vec3(1.0, 0.0, 0.0));
 
-		color -= color * circ1;
+	float side = draw_band(uv.y, 0.9, 0.95, 0.0, 1.0);
+	color -= side*color;
+	color += set_color(side, vec3(0.0, 0.2, 0.0));
 
-		color += set_color(circ1, vec3(uv.x, 0.5, 0.2));
-	}
-
+	float rect = draw_rect(uv, vec2(0.6, 0.6), 0.1, 0.1, 0.0, 1.0);
+	color -= rect*color;
+	color += set_color(rect, vec3(0.0, 1.0, 0.9));
 
 	gl_FragColor = vec4(color, 1.0);
 }
